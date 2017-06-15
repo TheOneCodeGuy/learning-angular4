737af3271fe452f4dc58981981dce7eedd101596
Renganathan <renganathan.1998@gmail.com>
2017-06-15 22:47:20 +0530

    Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import  { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    ProductComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+    	{
+    		path : 'member',
+    		component : MembersComponent 
+    	},
+    	{
+    		path : 'product',
+    		component : ProductComponent 
+    	},
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
