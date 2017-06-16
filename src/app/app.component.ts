b0c155c881f2373d57f3effcf643f4ef6c10ea9b
Renganathan <renganathan.1998@gmail.com>
2017-06-16 10:29:18 +0530

    Simple Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Rengu';
-  obj = {
-  	id: 1,
-  	name: "rengu"
-  };
-  arr = [
-  	{ id: 1, name: "rengu"},
-  	{ id: 4, name: "Raju"}
-  ];
-  styleObj = {
-  	"color" : "red"
+  items = ["Angular4", "React", "Underscore"];
+  newItem = "";
+  pushItem = function() {
+  	if(this.newItem != "")
+  		{
+  			this.items.push(this.newItem);
+  			this.newItem = "";
+  		}
+  }
+  removeItem = function(index){
+  	this.items.splice(index, 1)
   }
 }
